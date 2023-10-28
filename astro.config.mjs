d96a9917be3b90ff16954647c9f3f868ada2e649
akara <55230837+richeyphu@users.noreply.github.com>
2023-10-28 19:15:03 +0700

    Add PWA support (#2)
    * feat: support pwa
    
    * remove service worker
    
    * update favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,48 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(),
+    react(),
+    AstroPWA({
+      manifest: {
+        name: "chordsource",
+        short_name: "chord",
+        description: "เครื่องมือหาคอร์ดจากเว็บต่าง ๆ",
+        theme_color: "#13151a",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
